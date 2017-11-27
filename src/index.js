3bf95e37a2eee55ca64edaa2cc42bb2fff48f174
Allen Cheng <ac@allencheng.me>
2017-11-26 16:01:19 -0800

    Set up initial project with Babel ES6/Flow

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// @flow
+
+const token = process.env.SLACK_API_TOKEN || '';
+
+console.log(token);
+
+function gg(x: number): string {
+	return '';
+}
+
+function hh(s: string): number {
+	return 1;
+}
+
+async function g(s: string): Promise<string> {
+	return Promise.resolve('');
+}
